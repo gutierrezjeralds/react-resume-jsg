c0fe023ae81dc65b49df25823e5c3bf7aab4b905
Jerald Gutierrez <jerald.gutierrez@thinkconveyancing.com.au>
2019-12-05 07:26:43 +0800

    Commit changes for react reveal when srcolling

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { MDBBox, MDBContainer, MDBRow, MDBCol } from "mdbreact"
 import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView } from "mdbreact"
-import queryString from 'query-string'
+import { Fade } from 'react-reveal';
 import Banner from './Banner'
 
 class Home extends React.Component {
@@ -35,38 +35,40 @@ class Home extends React.Component {
         return (
             <MDBBox tag="div" className="home-wrapper">
                 <Banner wrapper="home" />
-                <MDBContainer fluid className="py-5 position-relative home-about-content">
-                    <MDBContainer>
-                        <MDBRow className="justify-content-center">
-                            <MDBCol md="12" className="mb-3">
-                                <MDBBox tag="span" className="content-title d-block font-size-3rem font-family-fantasy text-center">My Passions &amp; Personality</MDBBox>
-                            </MDBCol>
-                            {
-                                this.state.carousel !== "" ? (
-                                    <MDBCol lg="4" className="d-none d-lg-block home-carousel">
-                                        <MDBCarousel activeItem={this.state.carousel[0].id} length={this.state.carousel.length} showControls={false} showIndicators={false} slide>
-                                            <MDBCarouselInner>
-                                                {
-                                                    this.state.carousel.map(items => (
-                                                        <MDBCarouselItem key={items.id} itemId={items.id}>
-                                                            <MDBView>
-                                                                <img className="d-block w-100" src={items.src} alt={items.alt} style={{height: "400px"}} />
-                                                            </MDBView>
-                                                        </MDBCarouselItem>
-                                                    ))
-                                                }
-                                            </MDBCarouselInner>
-                                        </MDBCarousel>
+                    <MDBContainer fluid className="py-5 position-relative home-about-content">
+                        <Fade bottom>
+                            <MDBContainer>
+                                <MDBRow className="justify-content-center">
+                                    <MDBCol md="12" className="mb-3">
+                                        <MDBBox tag="span" className="content-title d-block font-size-3rem font-family-fantasy text-center">My Passions &amp; Personality</MDBBox>
                                     </MDBCol>
-                                ) : ( "" )
-                            }
-                            <MDBCol lg="6" md="12">
-                                <MDBBox tag="span" className="content-sub-title d-block font-size-2rem font-weight-light">A few fun facts about myself</MDBBox>
-                                <MDBBox tag="p" clas="content-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</MDBBox>
-                            </MDBCol>
-                        </MDBRow>
+                                    {
+                                        this.state.carousel !== "" ? (
+                                            <MDBCol lg="4" className="d-none d-lg-block home-carousel">
+                                                <MDBCarousel activeItem={this.state.carousel[0].id} length={this.state.carousel.length} showControls={false} showIndicators={false} slide>
+                                                    <MDBCarouselInner>
+                                                        {
+                                                            this.state.carousel.map(items => (
+                                                                <MDBCarouselItem key={items.id} itemId={items.id}>
+                                                                    <MDBView>
+                                                                        <img className="d-block w-100" src={items.src} alt={items.alt} style={{height: "400px"}} />
+                                                                    </MDBView>
+                                                                </MDBCarouselItem>
+                                                            ))
+                                                        }
+                                                    </MDBCarouselInner>
+                                                </MDBCarousel>
+                                            </MDBCol>
+                                        ) : ( "" )
+                                    }
+                                    <MDBCol lg="6" md="12">
+                                        <MDBBox tag="span" className="content-sub-title d-block font-size-2rem font-weight-light">A few fun facts about myself</MDBBox>
+                                        <MDBBox tag="p" clas="content-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</MDBBox>
+                                    </MDBCol>
+                                </MDBRow>
+                            </MDBContainer>
+                        </Fade>
                     </MDBContainer>
-                </MDBContainer>
                 <MDBContainer fluid style={{background: "white"}}>
                     <div style={{height: "100vh"}}></div>
                 </MDBContainer>
