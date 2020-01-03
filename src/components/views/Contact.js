f8099c42f7eb28e2687ab8c3e4657e5f2e15a90e
Jerald Gutierrez <jerald.gutierrez@thinkconveyancing.com.au>
2020-01-03 08:42:37 +0800

    reCaptcha configuration

--- src/components/views/Contact.js
+++ src/components/views/Contact.js
@@ -70,11 +70,47 @@ class Contact extends React.Component {
 
     handleSubmit (event) {
         event.preventDefault();
-        this.sendMessage()
+        this.getReCaptchaRes()
     }
 
-    sendMessage () {
-        const data = {
+    getReCaptchaRes() {
+        const reCaptchaData = {
+            "secret": "6Let3csUAAAAANhYJr1yZINe-G7ZZXPP1rPCvXZS",
+            "response": "6Let3csUAAAAACNBGTJLupV20O3iRi2DXNbj-eUV"
+        }
+
+        $.ajax({
+            url: "https://www.google.com/recaptcha/api/siteverify",
+            type: 'POST',
+            data: JSON.stringify(reCaptchaData),
+            contentType: 'application/json',
+            cache: false
+        })
+        .then(
+            (result) => {
+                console.log(result)
+                // this.sendMessage()
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                // Handle errors here
+                console.error('Oh well, you failed. Here some thoughts on the error that occured:', error)
+                alert("Unexpected error, please reload the page!")
+            }
+        )
+        .catch(
+            (err) => {
+                // Handle errors here
+                console.error('Oh well, you failed. Here some thoughts on the error that occured:', err)
+                alert("Unexpected error, please reload the page!")
+            }
+        )
+    }
+
+    sendMessage(reCaptchaRes) {
+        const eDMData = {
             service_id: "gmail",
             template_id: "template_gwqFwjqA",
             user_id: "user_DKcMwG40VRnkIFionziRA",
@@ -82,14 +118,15 @@ class Contact extends React.Component {
                 "from_name": this.state.in_name,
                 "reply_to": this.state.in_email,
                 "subject": this.state.in_subject,
-                "message": this.state.in_message
+                "message": this.state.in_message,
+                "g-recaptcha-response": reCaptchaRes
             }
         }
 
         $.ajax({
             url: "https://api.emailjs.com/api/v1.0/email/send",
             type: 'POST',
-            data: JSON.stringify(data),
+            data: JSON.stringify(eDMData),
             contentType: 'application/json',
             cache: false
         })
@@ -144,6 +181,7 @@ class Contact extends React.Component {
                                                 </MDBBox>
                                             ))
                                         }
+                                        <MDBBox tag="div" className="g-recaptcha md-form flex-center" data-sitekey="6Let3csUAAAAANhYJr1yZINe-G7ZZXPP1rPCvXZS"></MDBBox>
                                         <MDBBox tag="div" className="text-center">
                                             <MDBBtn type="submit" color="primary">
                                                 <MDBIcon icon="paper-plane" className="mr-2" />
