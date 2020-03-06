b805b0cea704f40c42fe6859dca2dde5e76d56e7
Jerald Gutierrez <jerald.gutierrez@thinkconveyancing.com.au>
2020-03-06 10:29:36 +0800

    Uncomment serviceWorker

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './components/assets/sass/screen.scss'
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
+serviceWorker.register();
